e4a577afaea939c9b3d0013bc97e110d980a507a
Ruairi <G00411334@gmail.ie>
2022-03-25 22:03:16 +0000

    mar25th

--- analysis.py
+++ analysis.py
@@ -1,1 +1,10 @@
 # Performing analysis on Fisher's Iris dataset.
+
+# Pandas built-in module can be used to output dataframes:
+
+import pandas as pd
+data = pd.read_csv('iris.data')
+print(data.loc[::30])
+
+df = pd.DataFrame(data, columns=[1, 2, 3, 4, 5])
+print(df)
