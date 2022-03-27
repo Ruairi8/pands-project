e298d631c8a1550ce5d6df2233344adfc22c562b
Ruairi <G00411334@gmail.ie>
2022-03-28 00:04:19 +0100

    scatterplot of the dataset using matlpotlib

--- analysis.py
+++ analysis.py
@@ -4,7 +4,22 @@
 
 import pandas as pd
 data = pd.read_csv('iris.data')
-print(data.loc[::30])
+# print(data.loc[::30])
+print(data.describe())
 
-df = pd.DataFrame(data, columns=[1, 2, 3, 4, 5])
-print(df)
+
+df = pd.DataFrame(data, columns=["SepalLength", "SepalWidth", "PetalLength", "PetalWidth", "Species"])
+# print(df)
+# print(df.head(12)) 
+print(data.loc[:, []])
+df.columns = ["SepalLength", "SepalWidth"]
+
+import numpy as np
+import matplotlib.pyplot as plt
+# x = 19
+# y = np.random.rand(x)
+# z = np.random.rand(x)
+plt.scatter(df["SepalLength"], df["SepalWidth"], df["PetalLength"], df["PetalWidth"])
+plt.yticks(np.arange(0, 6, 0.5))
+plt.xticks(np.arange(0, 6, 0.5))
+plt.show()
