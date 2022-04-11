9d0f398fdc7d3ccc9dfe5fe841757daf367c6792
Ruairi <G00411334@gmail.ie>
2022-04-11 23:34:58 +0100

    Updated README. Tidied up analysis.py

--- README.md
+++ README.md
@@ -2,7 +2,11 @@
 
 Author: Ruairi McCool
 
-In this project I will do meaningful analyses using python on the dataset, using some of it's built-in libraries. Visual distributions can be created using matplotlib and pandas libraries, for example. I could also divide the data into training and testing groups, as is done in "k nearest neighbours" in machine learning. I will aslo provide a written summary of the output and the conclusions I have made. 
+In this project I will output summaries of the variables involved to a text file and do meaningful analyses using python on the dataset using some of it's built-in libraries. Visual distributions can be created using matplotlib and pandas libraries, for example. I could also divide the data into training and testing groups, as is done in "k nearest neighbours" in machine learning. I will also provide a written summary of the output and the conclusions I have made.
+What can be gained by performing analysis and outputting mathematical statistics of the variables in this 
+dataset? The answer is that it can be determined which variables and combinations of them can be used to 
+distinguish each of the three varieties of iris from one another. It may be necessary to find out how reliable a method of distinguishing one variety of iris from another is, because not every method is one 
+hundred percent accurate and there may be a margin of error.
 
 <h4><em>Summary of the data set:</em></h4> 
 The Iris flower data set was introduced by Robert Fisher in 1936 in his paper <i>"the use of multiple measurements in taxonomic problems"</i> as an example of linear discriminant analysis. The data set contains fifty samples from each of three species of Iris (Setosa, Virginica and Versicolor). The columns contain the variables sepal length, sepal width, petal length, petal width measured in centimeters and species name. The rows contain the samples - 150 in total. Fisher was able to develop a linear discriminant model to distinguish the species from each other using this information. 
--- analysis.py
+++ analysis.py
@@ -1,62 +1,27 @@
 # Performing analysis on Fisher's Iris dataset.
 
 # Pandas built-in module can be used to output dataframes:
-from matplotlib.colors import Colormap
 import pandas as pd
 import numpy as np
+# Importing the dataset to data frame format using pandas .read_csv():
 irisData = pd.read_csv('iris.data')
-# This outputs only data only at certain locations, i.e. at the junction of a given step size:
-# print(data.loc[::30])
 
 # 'describe()' method outputs the count, standard deviation, mean, min & max and percentiles of a dataset:
 print(irisData.describe())
 # Creating a few numpy variables to print some maths statistics:
 x = np.mean(irisData["1"])
-print("The mean of the sepal length is: {}".format(x))
+print("The Mean Of The Sepal Length is: {}".format(x))
 y = np.std(irisData["1"])
-print("The standard deviation of sepal length is: {}".format(y))
+print("The Standard Deviation Of Sepal Length is: {}".format(y))
 z = np.min(irisData["1"])
-print("The minimum value for sepal length is: {}".format(z))
+print("The Minimum Value For Sepal length is: {}".format(z))
 x1 = np.max(irisData["1"])
-print("The maximum value for sepal length is: {}".format(x1))
-
-# https://stackoverflow.com/questions/64685561/how-to-write-print-statements-to-csv-file-in-python
-import csv 
-with open("VariableSummaries.txt", "w") as csv:
-    print("The mean of the sepal length is: {}".format(x), file=csv)
-    print("The standard deviation of sepal length is: {}".format(y), file=csv)
-    print("The minimum value of sepal length is: {}".format(z), file=csv)
-    print("The maximum value for sepal length is: {}".format(x1), file=csv)
-
-# x.to_csv('VariableSummaries.txt', sep='\t')
-# y.to_csv('VariableSummaries.txt', sep='\t')
-# z.to_csv('VariableSummaries.txt', sep='\t')
-# x1.to_csv('VariableSummaries.txt', sep='\t')
-
+print("The Maximum Value For Sepal Length Is: {}".format(x1))
 
 # 'df.values' must be emphasised to create a new dataframe with column names & to avoid missing values:
 df = pd.DataFrame(irisData.values, columns=["SepalLength", "SepalWidth", "PetalLength", "PetalWidth", "Species"])
+print(df.head(12)) 
 
-# print(df.head(12)) 
-# print(data.loc[:, []])
-# df.columns = ["SepalLength", "SepalWidth"]
-
-# Writing the dataframe from to a text file:
-# df.to_csv('VariableSummaries.txt', sep='\t')
-
-import seaborn as se
-import matplotlib.pyplot as plt
-# plt.hist(df1["SepalLength"], bins=8, color='green', alpha=0.5)
-# plt.show()
-# x = 19
-# y = np.random.rand(x)
-# z = np.random.rand(x)
-#plt.scatter(df1["SepalLength"], df1["SepalWidth"], Colormap["Species"])
-#plt.yticks(np.arange(1, 5, 1))
-#plt.xticks(np.arange(3, 10, 1))
-# plt.show()
-# se.scatterplot(x="PetalLength" "SepalLenght", y="PetalWidth" "SepalWidth", hue="Species", data=df)
-# plt.show()
 
 # The “loc” functions use the index name of the row to display the particular row of the dataset. 
 # The “iloc” functions use the index integer of the row, which gives complete information about the row.
@@ -69,24 +34,28 @@ iris_versicolor=df.loc[df["Species"]=="Iris-versicolor"]
 # Creating a few numpy variables to print some maths statistics:
 x2 = np.mean(iris_setosa)
 print("THE mean of the sepal length is: {}".format(x2))
-y2 = np.std(iris_virginica)
+y2 = np.std(iris_setosa)
 print("THE standard deviation of sepal length is: {}".format(y2))
 z2 = np.min(iris_setosa)
 print("THE minimum value for sepal length is: {}".format(z2))
 w = np.max(iris_setosa)
 print("THE maximum value for sepal length is: {}".format(w))
 
-total = x2 + y2 + z2 + w
-total.to_csv("VariableSummaries.txt", sep="\t")
 
 # https://eldoyle.github.io/PythonIntro/08-ReadingandWritingTextFiles/
 filename = "VariableSummaries.txt"
 file = open(filename, 'w')
+# .write() only takes a string as an argument. https://stackoverflow.com/questions/41454921/typeerror-write-argument-must-be-str-not-list
+file.write("Mean, Standard Deviation, Minimum & Maximim for Iris-Setosa\n")
+# https://www.w3resource.com/pandas/dataframe/dataframe-to_string.php
+# .to_string() 
+file.write("Mean of Sepal Length for all 3 irises: {}".format(str(x)))
+file.write(str(x2.to_string()))
 
-file.write("Sepal length mean is: {}".format(x2))
-file.write("Sepal length standard deviation is: {}".format(y2))
-file.write("Sepal length minimum value is: {}".format(z2))
-file.write("Sepal length maximum value is {}".format(w))
+file.write(str(y2.to_string()))
 
-file.close() 
+file.write(str(z2.to_string()))
+
+file.write(str(w.to_string()))
+
 
